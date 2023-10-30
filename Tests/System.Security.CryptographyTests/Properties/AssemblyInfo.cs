4f94db38d047c8c62dbb5beacff1ed5958e8b888
José Simões <jose.simoes@eclo.solutions>
2023-10-30 13:55:26 +0000

    Changes from review
    - Add license header.
    - Add description.

--- Tests/System.Security.CryptographyTests/Properties/AssemblyInfo.cs
+++ Tests/System.Security.CryptographyTests/Properties/AssemblyInfo.cs
@@ -1,16 +1,18 @@
-﻿using System.Reflection;
+﻿//
+// Copyright (c) .NET Foundation and Contributors
+// Portions Copyright (c) Microsoft Corporation.  All rights reserved.
+// See LICENSE file in the project root for full license information.
+//
+
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyCopyright("Copyright (c) 2021 nanoFramework contributors")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyDescription("Unit tests for HmacSha256 class")]
+[assembly: AssemblyCopyright("Copyright (c) .NET Foundation and Contributors")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
