d228673f599abdfcdddafb590acff20726c364c1
Alireza <alireza_paridar@hotmail.com>
2023-11-02 20:29:12 +0330

    add intellisesne comments for ECB

--- System.Security.Cryptography/AES.cs
+++ System.Security.Cryptography/AES.cs
@@ -10,7 +10,22 @@ namespace System.Security.Cryptography
     /// <summary>
     /// Specifies the block cipher mode to use for encryption.
     /// </summary>
-    public enum CipherMode { ECB = 2 }
+    public enum CipherMode 
+    {
+        /// <summary>
+        /// The Electronic Codebook (ECB) mode encrypts each block individually. Any blocks
+        /// of plain text that are identical and in the same message, or that are in a different
+        /// message encrypted with the same key, will be transformed into identical cipher
+        /// text blocks. Important: This mode is not recommended because it opens the door
+        /// for multiple security exploits. If the plain text to be encrypted contains substantial
+        /// repetition, it is feasible for the cipher text to be broken one block at a time.
+        /// It is also possible to use block analysis to determine the encryption key. Also,
+        /// an active adversary can substitute and exchange individual blocks without detection,
+        /// which allows blocks to be saved and inserted into the stream at other points
+        /// without detection.
+        /// </summary>
+        ECB = 2
+    }
 
     /// <summary>
     /// Encryption Standard (AES)
