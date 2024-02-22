d3779c3e9ff977164395af2aff25677f58b1c71c
Feiko <gorterfeiko@hotmail.com>
2024-02-22 17:48:57 +0100

    Added support for CipherMode.CBC

--- System.Security.Cryptography/CipherMode.cs
+++ System.Security.Cryptography/CipherMode.cs
@@ -14,7 +14,18 @@ namespace System.Security.Cryptography
         /// No cipher mode set. 
         /// </summary>
         None = 0,
-
+        /// <summary>
+        /// The Cipher Block Chaining (CBC) mode introduces feedback. Before each plain text
+        /// block is encrypted, it is combined with the cipher text of the previous block
+        /// by a bitwise exclusive OR operation. This ensures that even if the plain text
+        /// contains many identical blocks, they will each encrypt to a different cipher
+        /// text block. The initialization vector is combined with the first plain text block
+        /// by a bitwise exclusive OR operation before the block is encrypted. If a single
+        /// bit of the cipher text block is mangled, the corresponding plain text block will
+        /// also be mangled. In addition, a bit in the subsequent block, in the same position
+        /// as the original mangled bit, will be mangled.
+        /// </summary>
+        CBC = 1,
         /// <summary>
         /// The Electronic Codebook (ECB) mode encrypts each block individually. Any blocks
         /// of plain text that are identical and in the same message, or that are in a different
