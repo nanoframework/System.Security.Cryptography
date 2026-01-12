bbf8d80ce50ba9155f5fa60294d08299f943d261
José Simões <jose.simoes@eclo.solutions>
2026-01-12 10:39:20 +0000

    Migrate mscorlib to v2
    - Update references.
    - Bump versions.
    - Update AZDO and nuspec files.

--- System.Security.Cryptography/Properties/AssemblyInfo.cs
+++ System.Security.Cryptography/Properties/AssemblyInfo.cs
@@ -16,5 +16,5 @@ using System.Runtime.InteropServices;
 
 ////////////////////////////////////////////////////////////////
 // update this whenever the native assembly signature changes //
-[assembly: AssemblyNativeVersion("100.0.0.3")]
+[assembly: AssemblyNativeVersion("100.2.0.3")]
 ////////////////////////////////////////////////////////////////
